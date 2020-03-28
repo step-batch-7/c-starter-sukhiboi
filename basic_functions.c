dc03e173e392b6c97cc4f81f713e2ff991b1ad77
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-03-28 16:47:37 +0530

    Solved isOdd and isEven problems

---
+++ basic_functions.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int getIntegerFromUser(void);
+unsigned char is_even(int);
+void is_even_main(void);
+void is_odd_main(void);
+
+int getIntegerFromUser(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    return num;
+}
+
+unsigned char is_even(int num)
+{
+    return num % 2 == 0;
+}
+
+unsigned char is_odd(int num)
+{
+    return !is_even(num);
+}
+
+void is_even_main(void)
+{
+    printf("\nisEven\n----\n");
+    int num = getIntegerFromUser();
+    printf("%d is %s\n", num, is_even(num) ? "even" : "not even" );
+}
+
+void is_odd_main(void)
+{
+    printf("\nisOdd\n----\n");
+    int num = getIntegerFromUser();
+    printf("%d is %s\n", num, is_odd(num) ? "odd" : "not odd" );
+}
+
+int main(void)
+{
+    is_even_main();
+    is_odd_main();
+    return 0;
+}
