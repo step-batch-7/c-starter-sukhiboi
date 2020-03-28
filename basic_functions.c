7409fc0b17917893645da4bedb4d92f851107842
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-03-28 16:59:37 +0530

    Solved square and cube problem

--- basic_functions.c
+++ basic_functions.c
@@ -4,8 +4,13 @@ int getIntegerFromUser(void);
 
 unsigned char is_even(int);
 unsigned char is_odd(int);
+int square(int);
+int cube(int);
+
 void is_even_main(void);
 void is_odd_main(void);
+void square_main(void);
+void cube_main(void);
 
 int getIntegerFromUser(void)
 {
@@ -25,6 +30,16 @@ unsigned char is_odd(int num)
     return !is_even(num);
 }
 
+int square(int num)
+{
+    return num * num;
+}
+
+int cube(int num)
+{
+    return square(num) * num;
+}
+
 void is_even_main(void)
 {
     printf("\nisEven\n----\n");
@@ -39,9 +54,25 @@ void is_odd_main(void)
     printf("%d is %s\n", num, is_odd(num) ? "odd" : "not odd" );
 }
 
+void square_main(void)
+{
+    printf("\nsquare\n----\n");
+    int num = getIntegerFromUser();
+    printf("Square of %d is %d\n", num, square(num));
+}
+
+void cube_main(void)
+{
+    printf("\ncube\n----\n");
+    int num = getIntegerFromUser();
+    printf("Cube of %d is %d\n", num, num * square(num));
+}
+
 int main(void)
 {
     is_even_main();
     is_odd_main();
+    square_main();
+    cube_main();
     return 0;
 }
