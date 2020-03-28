dc26b4d23170ee521944468fcf86235b566f5e5c
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-03-28 21:20:09 +0530

    Solved problem of fibonacci series with loops

--- loops.c
+++ loops.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 
 int factorial(int);
+int fibonacci(int);
 
 void test_factorial(void);
+void test_fibonacci(void);
 
-int factorial(int num) 
+int factorial(int num)
 {
     int fact = 1;
-    while (num > 1)
+    int limit = num;
+    while (limit > 1)
     {
-        fact *= num;
-        num--;
+        fact *= limit;
+        limit--;
     }
     return fact;
 }
 
-void test_factorial(void) 
+int fibonacci(int termCount)
+{
+    int count = 0;
+    int first_element = -1;
+    int second_element = 1;
+    int current_element;
+    while (count < termCount)
+    {
+        current_element = first_element + second_element;
+        first_element = second_element;
+        second_element = current_element;
+        printf("%d ", current_element);
+        count++;
+    }
+    return current_element;
+}
+
+//tests
+
+void test_factorial(void)
 {
     int num = 10;
-    printf("Factorial of %d is %d", num, factorial(num));
+    printf("Factorial of %d is %d\n", num, factorial(num));
+}
+
+void test_fibonacci(void)
+{
+    int termCount = 10;
+    printf("First %d terms of fibonacci series are ", termCount);
+    fibonacci(termCount);
 }
 
 int main(void)
 {
     test_factorial();
+    test_fibonacci();
     return 0;
 };
