e102b2a7a738a1901eca9fff258c7c3c94f14737
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-03-28 21:30:24 +0530

    Solved problem of odd number series from 1 to n

--- loops.c
+++ loops.c
@@ -2,9 +2,13 @@
 
 int factorial(int);
 int fibonacci(int);
+unsigned char is_even(int);
+unsigned char is_odd(int);
+int oddNumberSeries(int);
 
 void test_factorial(void);
 void test_fibonacci(void);
+void test_oddNumberSeries(void);
 
 int factorial(int num)
 {
@@ -35,6 +39,31 @@ int fibonacci(int termCount)
     return current_element;
 }
 
+unsigned char is_even(int num)
+{
+    return num % 2 == 0;
+}
+
+unsigned char is_odd(int num)
+{
+    return !is_even(num);
+}
+
+int oddNumberSeries(int limit) {
+    int current_element;
+    int count = 1;
+    while (count < limit)
+    {
+        current_element = count;
+        if (is_odd(current_element))
+        {
+            printf("%d ", current_element);
+        }
+        count++;
+    }
+    return current_element;
+}
+
 //tests
 
 void test_factorial(void)
@@ -48,11 +77,21 @@ void test_fibonacci(void)
     int termCount = 10;
     printf("First %d terms of fibonacci series are ", termCount);
     fibonacci(termCount);
+    printf("\n");
+}
+
+void test_oddNumberSeries(void)
+{
+    int limit = 10;
+    printf("Odd terms between 1 and %d are ", limit);
+    oddNumberSeries(limit);
+    printf("\n");
 }
 
 int main(void)
 {
     test_factorial();
     test_fibonacci();
+    test_oddNumberSeries();
     return 0;
 };
